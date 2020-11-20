4d78b3f8c54afb4d20b4425ca38b668e65e18618
David-Egea <201805328@alu.comillas.edu>
2020-11-20 13:36:17 +0100

    git push

---
+++ app/src/main/java/com/example/closet/util/Util.java
@@ -0,0 +1,14 @@
+package com.example.closet.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+public class Util {
+
+    private static HashMap<String, ArrayList<String>> campos;
+
+    public static HashMap<String,ArrayList<String>> getMap() { return campos;}
+
+
+}

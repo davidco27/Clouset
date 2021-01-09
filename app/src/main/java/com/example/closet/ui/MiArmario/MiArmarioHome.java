a7ccd9ebbc3ebc0d672a09cb776d2a173a9dab6c
davidco27 <74071922+davidco27@users.noreply.github.com>
2021-01-09 10:42:42 +0100

    adfsfasfds

--- app/src/main/java/com/example/closet/ui/MiArmario/MiArmarioHome.java
+++ app/src/main/java/com/example/closet/ui/MiArmario/MiArmarioHome.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -31,23 +32,19 @@ public class MiArmarioHome extends Fragment {
     private static ArrayList<Outfit> outfits;
     private  static ListView lista;
     private  static Activity activity;
+    private  static Button btnPrendas,btnOutfits;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.miarmario_main, container, false);
-
-            /*
-            SPRINT3 OUTFITS
-            outfits = Client.conectarseBD("/getOutfit", null, "", 0);
-             */
-
-        outfits = null;
-
+         btnPrendas = view.findViewById(R.id.btnPrendas);
+         btnOutfits = view.findViewById(R.id.btnOutfits);
         return view;
     }
     public static void actualizarLista(){
         usuario = MainActivity.getUsuario();
         prendas = Client.conectarseBD("/getPrenda", null, "",0,usuario,activity);
+        outfits = Client.conectarseBDOutfits("/getOutfit", 0, null, activity,usuario);
         lista.setAdapter(new PrendaAdapter(activity, R.layout.support_simple_spinner_dropdown_item, prendas));
     }
 
@@ -57,7 +54,22 @@ public class MiArmarioHome extends Fragment {
         activity=getActivity();
         lista = view.findViewById(R.id.listaPrendas);
         actualizarLista();
-
+        btnOutfits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnOutfits.setVisibility(View.INVISIBLE);
+                btnPrendas.setVisibility(View.VISIBLE);
+                //lista.setAdapter(new OutfitAdapter(activity, R.layout.support_simple_spinner_dropdown_item, prendas));
+            }
+        });
+        btnPrendas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnOutfits.setVisibility(View.VISIBLE);
+                btnPrendas.setVisibility(View.INVISIBLE);
+                lista.setAdapter(new PrendaAdapter(activity, R.layout.support_simple_spinner_dropdown_item, prendas));
+            }
+        });
     }
 
     public static ArrayList<Prenda> getPrendasMiArmario() { return prendas;}
